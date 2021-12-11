50b413d2285ad1a0287ce06d4ea42bc163b70d9a
michelearias <66805958+michelearias@users.noreply.github.com>
2021-12-10 23:45:58 -0300

    Inclusão de novos exercícios

---
+++ aula050 (jogo da forca1)/forca.js
@@ -0,0 +1,14 @@
+var biblioteca=['javascript','curso','computador', 'transporte', 'livraria', 'tecnologia', 'controle', 'churrasco', 'brasil', 'impressora', 'xicara', 'telefone', 'internet', 'camera', 'monitor', 'brinquedo', 'youtube', 'portaria', 'escola', 'carnaval', 'teclado', 'guitarra', 'bateria', 'chinelo', 'helicoptero', 'arduino']
+
+var qtde=biblioteca.length-1
+var pos=Math.round(Math.random()*qtde)
+var palavra=biblioteca[pos]
+var tam=palavra.length
+var cxLetras=[]
+var acertos
+var errosMax=7
+var erros=0
+var desenhos=[]
+var acertou=false
+var jogando=false
+
